647f8243c9ef9d8fa34bd8a383c22d87169e423e
Jamile Guedes <jamile.guedes@zoly.corp>
2020-02-03 19:02:46 -0300

    correcoes1

--- script.js
+++ script.js
@@ -3,10 +3,10 @@
 
 	   document.querySelector(botaoseta).addEventListener('click', function(){alert({{dataLayer--meuNome}});
 	});
-	   
+
 	   window.dataLayer[
-       'category': 'Botão'
-       'action': 'click'
+       'category': 'Botão',
+       'action': 'click',
        'label': botaoseta;
        ];
         
