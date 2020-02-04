50e35d293ec3899fabb490901c37b04eca343797
Jamile Guedes <jamile.guedes@zoly.corp>
2020-02-04 15:40:33 -0300

    footer1

--- script.js
+++ script.js
@@ -10,15 +10,5 @@ function addEventos(){
 
   //myNewName.push({'variable_name': 'variable_value'});
 
-// window.AddEventListener("load",addEventos);
 
-document.querySelector('.dados').addEventListener('click', function(){
-var a = window.prompt('Qual seu nome ?');
-var b = window.prompt('Qual sua idade ?');
-dataLayer.push({
-'event':'event',
-'eventCategory':'category',
-'eventAction':a,
-'eventLabel':b,
-});
-});
+
