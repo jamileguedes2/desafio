94f34d7846b0f1600a9209aa26bdeef1af8bf9f4
Jamile Guedes <jamile.guedes@zoly.corp>
2020-02-04 15:13:09 -0300

    teste

--- script.js
+++ script.js
@@ -8,18 +8,8 @@ function addEventos(){
 
 }
 
-window.AddEventListener("load",addEventos);
-
   //myNewName.push({'variable_name': 'variable_value'});
 
-  function phone(){
-	document.getElementById("caixaPhone").AddEventListener("click",a, b); 
+  
 
-  	var a = window.prompt("Qual o seu nome de terráqueo ?");
-  	var b = window.prompt("Qual a sua idade de terráqueo ?");
-  	dataLayer.push({
- 	'event': 'event',  
-    'eventName': a,
-    'eventAge': b,
-  });
-  console.log(a+b);
+window.AddEventListener("load",addEventos);
