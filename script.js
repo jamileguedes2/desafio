6ba87546b0d7dbc5cc292650a0f3f3653dfed1dc
Jamile Guedes <jamile.guedes@zoly.corp>
2020-02-04 15:50:48 -0300

    A maiusculo do add

--- script.js
+++ script.js
@@ -10,5 +10,14 @@ function addEventos(){
 
   //myNewName.push({'variable_name': 'variable_value'});
 
-
+document.querySelector('.dados').AddEventListener('click', function()){
+     var a = window.prompt('Qual seu nome ?');
+       var b = window.prompt('Qual sua idade ?');
+       dataLayer.push({
+                'event':'event',
+                'eventCategory':'category',
+                'eventAction':a,
+                'eventLabel':b,
+                });
+                });
 
