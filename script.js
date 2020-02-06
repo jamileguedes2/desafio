d6b734a2259d5c930e7a3bc9eda513013a0c6a24
Jamile Guedes <jamile.guedes@zoly.corp>
2020-02-06 15:55:50 -0300

    comentando o script do click logo

--- script.js
+++ script.js
@@ -1,13 +1,6 @@
 
  document.addEventListener("DOMContentLoaded", function(event) {
 
-
-
-
-document.querySelector('#logoid').addEventListener("click", "") //function(){ alert("Seja bem vindo!"); 
-//});
-
-
 	document.querySelector('#dados').addEventListener('click', function(){
 		var a = window.prompt('Qual seu nome ?');
 		var b = window.prompt('Qual sua idade ?');
